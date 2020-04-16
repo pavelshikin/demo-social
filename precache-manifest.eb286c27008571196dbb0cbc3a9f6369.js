68a5da6dfb46729b0c94bc18fb1dd29a1465b9c6
Pavel <sh1k@bk.ru>
2020-04-16 15:24:34 +0300

    Updates

---
+++ precache-manifest.eb286c27008571196dbb0cbc3a9f6369.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f44a97b5fa69a964b1d585910dcbd1",
+    "url": "/demo-social/index.html"
+  },
+  {
+    "revision": "76ae1e92e57a947001b4",
+    "url": "/demo-social/static/css/3.a11b1a38.chunk.css"
+  },
+  {
+    "revision": "41a758d8da13c6fe5591",
+    "url": "/demo-social/static/css/4.4880efcf.chunk.css"
+  },
+  {
+    "revision": "4e12699ec8a535c853d2",
+    "url": "/demo-social/static/css/main.c23a3c6c.chunk.css"
+  },
+  {
+    "revision": "f73a7cfb93847adbd985",
+    "url": "/demo-social/static/js/2.cf93282e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-social/static/js/2.cf93282e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ae1e92e57a947001b4",
+    "url": "/demo-social/static/js/3.9fde854f.chunk.js"
+  },
+  {
+    "revision": "41a758d8da13c6fe5591",
+    "url": "/demo-social/static/js/4.7a9055b9.chunk.js"
+  },
+  {
+    "revision": "4e12699ec8a535c853d2",
+    "url": "/demo-social/static/js/main.d840bff3.chunk.js"
+  },
+  {
+    "revision": "a6b61a9539a3c50e4f20",
+    "url": "/demo-social/static/js/runtime-main.94f97754.js"
+  },
+  {
+    "revision": "1c997e591274efe17c082199f4abdf00",
+    "url": "/demo-social/static/media/userphoto.1c997e59.png"
+  }
+]);
